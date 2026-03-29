214690fa248636e32acc02e951be1fc7fad14db4
Pablo <50000234+Pablo923@users.noreply.github.com>
2026-03-29 00:45:16 -0300

    Update WiFi component and configuration. First funciontional test.
    - Added WiFi component with initialization and connection functions.
    - Updated main application to include WiFi functionality and handle connection status.
    - Modified CMakeLists.txt to include new WiFi component dependencies.
    - Updated VSCode settings for the correct serial port.
    - Added new entries to .gitignore for sdkconfig files.
    - Updated README.md with WiFi configuration instructions.

---
+++ components/wifi/include/wifi.h
@@ -0,0 +1,70 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+/**
+ * @brief Event group bits for WiFi events
+ */
+#define WIFI_STA_CONNECTED_BIT      BIT0
+#define WIFI_STA_IPV4_OBTAINED_BIT  BIT1
+#define WIFI_STA_IPV6_OBTAINED_BIT  BIT2
+
+// Event group handle for WiFi events. Set by wifi_sta_init() if event_group is NULL.
+extern EventGroupHandle_t network_event_group;
+
+/**
+ * @brief Configuration WiFi
+ * 
+ * This function initializes the WiFi driver and sets up event handlers for WiFi
+ * and IP events. It does not start the WiFi driver or connect to a network -
+ * call wifi_sta_init() to do that.
+ * 
+ * Important! you must call esp_netif_init() and esp_event_loop_create_default()
+ * before calling this function.
+ * 
+ * @return
+ *  - ESP_OK on success
+ *  - Other errors on failure. See esp_err.h for error codes.
+ */
+void wifi_init(void);
+
+/**
+ * @brief Initialize WiFi in station (STA) mode.
+ * 
+ * Set up the WiFi interface and connect to a WiFi network. You can use the
+ * event group to wait for a connection and IP address assignment. 
+ * 
+ * Important! you must call esp_netif_init() and esp_event_loop_create_default()
+ * before calling this function.
+ * 
+ * @param[in] event_group Event group handle for WiFi and IP events. Pass NULL
+ *                        to use the existing event group.
+ * 
+ * @return
+ *  - ESP_OK on success
+ *  - Other errors on failure. See esp_err.h for error codes.
+ */
+esp_err_t wifi_sta_init(EventGroupHandle_t event_group);
+
+/**
+ * @brief Disable WiFi
+ * 
+ * @return
+ *  - ESP_OK on success
+ *  - Other errors on failure. See esp_err.h for error codes.
+ */
+esp_err_t wifi_sta_stop(void);
+
+/**
+ * @brief Attempt to reconnect WiFi
+ * 
+ * @return
+ *  - ESP_OK on success
+ *  - Other errors on failure. See esp_err.h for error codes.
+ */
+esp_err_t wifi_sta_reconnect(void);
+
+#endif // WIFI_H
